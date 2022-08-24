5bebf9e74e732ee0ddf0ca3a1cb3c8f01e18d890
Nathan Hood <nathanjhood@googlemail.com>
2022-08-24 03:26:05 +0200

    Implemented initial source files

---
+++ src/main.cpp
@@ -0,0 +1,68 @@
+﻿/*******************************************************************************
+ *
+ * @file main.cpp
+ *
+ * @author Nathan J. Hood (nathanjhood@googlemail.com)
+ * @version 1.0.0.1
+ * @date 2022-08-13
+ *
+ * @copyright Copyright (c) 2022 @StoneyDSP
+ *
+ *******************************************************************************
+*/
+
+#include "main.h"
+
+/** Main.
+ * @brief Entry point for program execution.
+ *
+ * @param argc int
+ * @param argv char**
+ * @param envp char**
+ * @return int
+ */
+int main(int argc, char* argv[], char* envp[])
+{
+    using namespace stoneydsp;
+
+    ignore(argc, argv, envp);
+
+    intro();
+
+    endFunction();
+
+    Double a{ 1 };
+
+    Double b{ 2 };
+
+    Double c{ 3 };
+
+    Double d{ a };
+
+    Double e{ 10 };
+
+    endFunction();
+
+    print(CENTRE, "Addition Tests");
+    lineBreak();
+    e = e + d + c + b + a;
+    endFunction();
+
+    print(CENTRE, "Subtraction Tests");
+    e = e - d - c - b - a;
+    endFunction();
+
+    print(CENTRE, "Multiplication Tests");
+    e = e * d * c * b * a;
+    endFunction();
+
+    print(CENTRE, "Division Tests");
+    e = e / d / c / b / a;
+    endFunction();
+
+    //print("Modulo Tests");
+    //e = e % d % c % b % a;
+    //endFunction();
+
+    return outro();
+}
---
+++ src/main.h
@@ -0,0 +1,30 @@
+﻿/*******************************************************************************
+ *
+ * @file main.h
+ *
+ * @author Nathan J. Hood (nathanjhood@googlemail.com)
+ * @version 1.0.0.1
+ * @date 2022-08-13
+ *
+ * @copyright Copyright (c) 2022 @StoneyDSP
+ *
+ *******************************************************************************
+*/
+
+#pragma once
+
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+// Config file before modules!!! (use Forced Include if possible(?))
+#include "../cfg/ConsoleApplication_Config.h"
+
+// Standard Library includes
+#include <iostream>
+#include <string.h>
+
+// 3rd Party Library includes
+#include <cppunit/Asserter.h>
+#include "modules/stoneydsp_core/stoneydsp_core.h"
+
+#endif // MAIN_H_INCLUDED
