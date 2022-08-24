4ccb501b95f35470eac1959b3a7bdf003e2baa0c
Nathan Hood <nathanjhood@googlemail.com>
2022-08-24 03:24:34 +0200

    Added config files for pre-processor

---
+++ cfg/ConsoleApplication_Config.h
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ *
+ * @file ConsoleApplication_Config.h.in
+ *
+ * @author Nathan J. Hood (nathanjhood@googlemail.com)
+ * @version 1.0.0.1
+ * @date 2022-08-13
+ *
+ * @copyright Copyright (c) 2022 @StoneyDSP
+ *
+ *******************************************************************************
+*/
+
+#pragma once
+
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+
+/**
+ * @brief Defined project configuration information.
+ *
+ */
+#define companyName "StoneyDSP"
+#define projectName "ConsoleApplication"
+#define projectDescription "Console Application with CMake"
+#define projectVersion "v0.0.0.1"
+#define projectWebsite "https://stoneydsp.github.io/ConsoleApplication/html/index.html"
+
+#endif // CONFIG_H_INCLUDED
