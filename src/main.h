7d26e5e35c1ab2e5e77c52df483e0271ec3633ab
Nathan Hood <nathanjhood@googlemail.com>
2022-09-09 06:40:52 +0200

    Improving #includes

--- src/main.h
+++ src/main.h
@@ -29,7 +29,7 @@
 
 // 3rd Party Library includes
 #include <juce_core/juce_core.h>
-#include "../../StoneyDSP/modules/stoneydsp_core/stoneydsp_core.h"
+#include <stoneydsp_core.h>
 
 /** main.
  * @brief Entry point for program execution.
